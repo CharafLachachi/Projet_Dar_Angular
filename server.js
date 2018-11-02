be28a54d9ac4d937e009e15f3d9dc7b114b00b68
CharafLachachi <lac.charaf@gmail.com>
2018-11-02 12:18:43 +0100

    edit server.js add app name

--- server.js
+++ server.js
@@ -5,17 +5,18 @@ const path = require('path');
 const app = express();
 
 const path = require('path');
-
+// app.use(express.static(__dirname + '/dist/<name-of-app>'));
+// app.get('*', (req, res) => {
+//     res.sendFile(`./front-end/dist/index.html`); // load the single view file (angular will handle the page changes on the front-end)
+// });
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/<name-of-app>'));
-app.get('*', (req, res) => {
-    res.sendFile(`./front-end/dist/index.html`); // load the single view file (angular will handle the page changes on the front-end)
-});
-// app.get('/*', function(req,res) {
+app.use(express.static(__dirname + '/dist/voyageons-ensemble'));
+
+app.get('/*', function(req,res) {
     
-// res.sendFile(path.join(__dirname+'/dist/<name-of-app>/index.html'));
-// });
+res.sendFile(path.join(__dirname+'/dist/voyageons-ensemble/index.html'));
+});
 
 // Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
